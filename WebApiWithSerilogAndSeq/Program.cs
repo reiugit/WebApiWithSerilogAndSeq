f81d7067c057ac40cd4c3e22404846fc732d8058
rei <67372053+reiugit@users.noreply.github.com>
2024-07-27 19:24:50 +0200

    Projektdateien hinzufügen.

---
+++ WebApiWithSerilogAndSeq/Program.cs
@@ -0,0 +1,26 @@
+using Serilog;
+using Serilog.Context;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSerilog(x => x
+    .Enrich.WithProperty("_EnrichedWithProperty", "PropertyValue")
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .WriteTo.Seq("http://localhost:5341"));
+
+var app = builder.Build();
+
+app.UseSerilogRequestLogging();
+
+app.MapGet("/", (ILogger<Program> logger) =>
+{
+    using var logScope = logger.BeginScope("Scope with {ScopeValue}", "TestScopeValue");
+    using var logContext = LogContext.PushProperty("_EnrichedFromLogContext", "PushProperty");
+
+    logger.LogInformation(">>>> Sample log message from within the endpoint.");
+
+    return new { response = "Logs were sent to the Console and to Seq." };
+});
+
+app.Run();
